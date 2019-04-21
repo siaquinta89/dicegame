df9ef3e04aeef640ceb0b3d734b28743275d1b20
Stefania <siaquinta89@gmail.com>
2019-04-21 17:11:42 +0200

    test

--- app.js
+++ app.js
@@ -5,12 +5,23 @@ scores = [0,0];
 roundScore = 0;
 activePlayer = 1;
 
+document.querySelector('.dice').style.display='none';
 
-document.querySelector('#current-' + activePlayer).textContent = dice;
+document.getElementById('score-0').textContent='0';
+document.getElementById('score-1').textContent='0';
+document.getElementById('current-0').textContent='0';
+document.getElementById('current-1').textContent='0';
+
 
 document.querySelector('.btn-roll').addEventListener('click', function(){
 
     //Random number
     var dice = Math.floor(Math.random() *6 ) +1;
+
+    //Display the result
+    var diceDOM = document.querySelector('.dice');
+    diceDOM.style.display='block';
+    diceDOM.src = 'dice-' + dice + '.png';
+
 
 });
