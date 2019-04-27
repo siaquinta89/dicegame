f0cb1e919c9d7b7bf62bb9a3d97d2a7b1f5b74d3
Stefania Iaquinta <siaquinta89@gmail.com>
2019-04-27 12:36:24 +0200

    display block dado

--- app.js
+++ app.js
@@ -4,13 +4,19 @@ var scores, roundScore, activePlayer;
 scores = [0,0];
 roundScore = 0;
 activePlayer = 1;
+ 
+//Impostiamo il display none per il dado
+    document.querySelector('.dice').style.display = 'none';
+
+//Event Listener click
+    document.querySelector('.btn-roll').addEventListener('click', function(){
+        //1.Random number
+        var dice = Math.floor(Math.random() * 6) + 1;
 
 
-document.querySelector('#current-' + activePlayer).textContent = dice;
+        //2. Mostro il  risultato
+        var diceDOM = document.querySelector('.dice');
+        diceDOM.style.display='block';
+        diceDOM.src = 'dice-' + dice + '.png';
+    })
 
-document.querySelector('.btn-roll').addEventListener('click', function(){
-
-    //Random number
-    var dice = Math.floor(Math.random() *6 ) +1;
-
-});
