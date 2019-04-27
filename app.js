9d229c64409fff3d023a403713a24cd52f7bb44f
Stefania Iaquinta <siaquinta89@gmail.com>
2019-04-27 19:17:58 +0200

    modified something

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ activePlayer = 1;
  
 //Impostiamo il display none per il dado
     document.querySelector('.dice').style.display = 'none';
+    document.getElementById('score-0').textContent = '0';
+    document.getElementById('score-1').textContent = '0';
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0'
+
 
 //Event Listener click
     document.querySelector('.btn-roll').addEventListener('click', function(){
